2a26ed167e185ce98402c10a0bc6c1f6d53341b4
eqmvii <coelomate@gmail.com>
2017-10-12 12:55:17 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>React is rendering this text.</p>
+
+      </div>
+    );
+  }
+}
+
+export default App;
